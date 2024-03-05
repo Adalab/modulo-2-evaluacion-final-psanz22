a839b6dee9f1f289206ace0e3b10f8e3babc9182
Paula <psanzilundain@gmail.com>
2024-03-05 12:48:20 +0100

    favorites workinggit add -Agit add -Agit add -A

--- JS/main.js
+++ JS/main.js
@@ -26,12 +26,32 @@
 
 */
 
-let url = `c`;
+let url = ``;
 const buttonSearch = document.querySelector('.js-search');
 const inputSearch = document.querySelector('.js-input');
+let cardsList = [];
 const cardContainer = document.querySelector('.js-container-cards');
+const favContainer = document.querySelector('.js-container-favorites');
+let favoritesList = [];
 
-const renderCards = (cards) => {
+const handleAddFavorites = (event) => {
+  const clickedCardId = event.currentTarget.id;
+  // console.log('clickedCardId', clickedCardId);
+  // console.log('cardList', cardsList);
+
+  const cardSelected = cardsList.find((card) => {
+    // console.log('card.mal_id', card.mal_id);
+
+    return parseInt(clickedCardId) === card.mal_id;
+  });
+  console.log('cardSelected', cardSelected);
+
+  favoritesList.push(cardSelected);
+  console.log('favs', favoritesList);
+  renderCards(favoritesList, favContainer);
+};
+
+const renderCards = (cards, containerDOM) => {
   let html = '';
   for (const card of cards) {
     if (
@@ -39,32 +59,31 @@ const renderCards = (cards) => {
       'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
     ) {
       //   console.log('hol', card.images.jpg.image_url);
-      html += ` <div class="js-card"><img
+      html += ` <div class="js-card" id="${card.mal_id}"><img
     src="${card.images.jpg.image_url}"
     alt="${card.title}"
   />
   <h2>${card.title}</h2></div>`;
     } else {
-      html += `<div class="js-card"><img
+      html += `<div class="js-card" id="${card.mal_id}"><img
         src="https://i.redd.it/z3ldse72x7j61.png"
         alt="${card.title}"
       />
       <h2>${card.title}</h2></div>`;
     }
-    cardContainer.innerHTML = html;
-    // const cardElements = document.querySelectorAll('.js-card');
-
-    // for (const cardElement of cardElements) {
-    //   cardElements.addEventListener('click', handleFavorites);
-    // }
+    containerDOM.innerHTML = html;
+    const cardElements = document.querySelectorAll('.js-card');
+    for (const cardElement of cardElements) {
+      cardElement.addEventListener('click', handleAddFavorites);
+    }
   }
 };
 const fetching = () => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const cards = data.data;
-      renderCards(cards);
+      cardsList = data.data;
+      renderCards(cardsList, cardContainer);
     });
 };
 
