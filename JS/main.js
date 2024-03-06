da483f1ff12e327ddc322f3c5be31057690511c0
Paula <psanzilundain@gmail.com>
2024-03-06 12:29:09 +0100

    clean code

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,5 @@
 'use strict';
 
-/*
-    BUSCADOR DE SERIES DE ANIME
-
-    1. Estructura básica
-        - un campo te texto y un botón para buscar series por su título
-        - un listado de resultados de búsqueda donde aparece el cartel de la serie y el título
-
-    
-    2. Búsqueda
-        - al hacer click (evento) sobre el botón buscar
-            - hacemos fetch https://api.jikan.moe/v4/anime?q=${inputValue}, recoger input.value
-            - por cada serie el resultado pinta una tarjeta con la imagen de la serie y el título
-            - algunas no tienen imagen, detecta estos casos y ponles tú una imagen
-
-    3. Favoritos
-        - al hacer click(evento) sobre la card de la serie:
-            - cambiar estilo indicando que es favorita
-            - listado en la parte izquierda, bajo el inputSearch, con las favoritas
-            - deben seguir apareciendo aunque la usuaria realice otra búsqueda
-    
-    4. Almacenamiento local
-        - almacenar el listadoi de favoritos en el localStorage
-            - al recargar la página el listado de favoritos debe mostrarse
-
-*/
-
 let url = ``;
 const buttonSearch = document.querySelector('.js-search');
 const buttonReset = document.querySelector('.js-reset');
@@ -51,11 +25,11 @@ const handleAddFavorites = (event) => {
     favoritesList.splice(cardIndex, 1);
     event.currentTarget.classList.remove('fav');
   }
-  localStorage.setItem('favorites', JSON.stringify(favoritesList)); // string
+  localStorage.setItem('favorites', JSON.stringify(favoritesList));
 
   renderCards(favoritesList, favContainer);
 };
-const localStorageFavorites = JSON.parse(localStorage.getItem('favorites')); // Array
+const localStorageFavorites = JSON.parse(localStorage.getItem('favorites'));
 
 const renderCards = (cards, containerDOM) => {
   let html = '';
@@ -72,7 +46,6 @@ const renderCards = (cards, containerDOM) => {
       card.images.jpg.image_url !==
       'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
     ) {
-      //   console.log('hol', card.images.jpg.image_url);
       html += ` <div class="js-card ${
         isFavorite && !isFavoritesContainer ? 'fav' : ''
       }" id="${card.mal_id}"><img
